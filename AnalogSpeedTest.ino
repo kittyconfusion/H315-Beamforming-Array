2d9716e81229383776590a3a99f11a075d8a2aed
kittyconfusion <65476906+kittyconfusion@users.noreply.github.com>
2025-10-11 14:46:39 -0400

    Initial speed testing and POC for realtime FFT

---
+++ AnalogSpeedTest.ino
@@ -0,0 +1,70 @@
+// Determines the speed of the analogRead function for a variable number of microphones and the maximum polling rate
+#define MaximumNumberOfMics 8
+
+const uint16_t samples = 20000; // Samples taken each time
+unsigned long microseconds;
+
+struct Microphone {
+  double *vReal;
+  double *vImag;
+};
+
+Microphone mics[MaximumNumberOfMics];
+const unsigned int micPins[MaximumNumberOfMics] = {1, 3, 5, 6, 12, 14, 17, 18}; // first 4 on adc1 second 4 on adc2
+//const unsigned int micPins[MaximumNumberOfMics] = {1,12,3,14,5,17,6,18}; // alternating adc1 and adc2
+
+
+double *vReal;
+
+void setup()
+{
+  Serial.begin(115200);
+  while(!Serial);
+  Serial.println("Initialized");
+
+  Serial.print("Initial PSRAM: ");
+  Serial.println(ESP.getFreePsram());
+
+  for(int i = 0; i < MaximumNumberOfMics; i++) {
+    mics[i].vReal = (double *)ps_malloc(samples * sizeof(double));
+    mics[i].vImag = (double *)ps_malloc(samples * sizeof(double));
+  }
+
+  Serial.print("Free PSRAM after array allocations: ");
+  Serial.println(ESP.getFreePsram());
+
+  microseconds = micros();
+  for(int m = 0; m < MaximumNumberOfMics; m++) {
+    m++;
+
+    for(int i = 0; i < samples; i++) {
+      for(int j = 0; j < (m-1); j++) {
+        mics[j].vReal[i] = analogRead(micPins[j]); 
+        mics[j].vImag[i] = 0;
+      }
+    }
+
+    microseconds = micros() - microseconds;
+    double usPerSample = (microseconds * 1.0) / (samples * m);
+    double maxSampleRate = 1000000 / (usPerSample * m);
+    
+    Serial.println();
+    Serial.print(m);
+    Serial.println(" microphone(s)");
+    Serial.print("us per sample: ");
+    Serial.println(usPerSample);
+    Serial.print("max sampling rate: ");
+    Serial.println(maxSampleRate);
+
+    m--;
+  }
+}
+
+void loop()
+{
+  sleep(1);
+}
+
+//       /\_/\
+//      ( o.o )
+//       > ^ <
--- WhatsScreaming.ino
+++ WhatsScreaming.ino
@@ -6,79 +6,90 @@
 
 #include "arduinoFFT.h"
 
-#define NumberOfMics 2
+#define NumberOfMics 1
 
-const uint16_t samples = 128; // Samples before performing FFT, this value MUST ALWAYS be a power of 2
-const double maxFrequency = 1000; // Maxmimum frequency microphone will pick up
-const double samplingRate = 2*maxFrequency;
+const unsigned int samples = 1024; // Samples before performing FFT, this value MUST ALWAYS be a power of 2
+const double maxFrequency = 2000; // Maxmimum frequency microphone can pick up 
+const double samplingRate = 2*maxFrequency; //limited at 20k samples / # microphones
 
-unsigned int sampling_period_us; // How long between each sample
+const unsigned int sampling_period_us = round(1000000*(1.0/samplingRate)); // How long between each sample
 unsigned long microseconds;
 
 struct Microphone {
   double xPos; // to account for the physical difference in each microphone location
-  double vReal[samples];
-  double vImag[samples];
+  // These are the input and output for the FFT object
+  double *vReal;
+  double *vImag;
+  ArduinoFFT<double> FFT;
 };
 
 Microphone mics[NumberOfMics];
-const unsigned int micPins[NumberOfMics] = {A0, A1}; // separate from mics for faster dereferencing 
+const unsigned int micPins[NumberOfMics] = {6}; // separate from mics for faster dereferencing 
 
-/*
-These are the input and output vectors
-Input vectors receive computed results from FFT
-*/
-double *vReal;
-double *vImag;
-
-/* Create FFT object */
-ArduinoFFT<double> FFT = ArduinoFFT<double>(vReal, vImag, samples, samplingRate);
+unsigned int c = 0;
 
 void setup()
 {
-  sampling_period_us = round(1000000*(1.0/samplingRate));
   Serial.begin(115200);
   while(!Serial);
-  Serial.println("Initialized");
+  Serial.println("Initialized!!!!");
 
-  // each microphone in mics should be initialized here
+  Serial.print("Initial PSRAM: ");
+  Serial.println(ESP.getFreePsram());
+
+  for(int i = 0; i < NumberOfMics; i++) {
+    mics[i].vReal = (double *)ps_malloc(samples * sizeof(double));
+    mics[i].vImag = (double *)ps_malloc(samples * sizeof(double));
+    mics[i].FFT = ArduinoFFT<double>(mics[i].vReal, mics[i].vImag, samples, samplingRate);
+  }
+  Serial.print("Free PSRAM after array allocations: ");
+  Serial.println(ESP.getFreePsram());
 }
 
 void loop()
 {
   microseconds = micros();
-  for(int i=0; i<samples; i++) {
-    for(int j=0; j<NumberOfMics; j++) {
-      // pauses execution for ~100us (10kHz sampling rate of a single microphone)
-      // this may need to be accounted for because it adds up very fast
-      mics[j].vReal[i] = analogRead(micPins[j]); 
+  for(int i = 0; i < samples; i++) {
+    for(int j = 0; j < NumberOfMics; j++) {
+      // pauses execution for ~45-48us for 1-8 microphones respectively storing in psram
+      mics[j].vReal[i] = analogRead(micPins[j]);  // 12-bit resolution
       mics[j].vImag[i] = 0; 
     }
 
-    // at 2kHz sampling rate waits for 500us to have passed
-    while(micros() - microseconds < sampling_period_us) {
-      //empty loop
+  // at 2kHz sampling rate waits for 500us to have passed
+  while(micros() - microseconds < sampling_period_us) {
+    //empty loop
     }
-    microseconds += sampling_period_us;
+  microseconds += sampling_period_us;
   }
+  usleep(100000);
+  //Serial.println(++c);
+  for(int k = 0; k < NumberOfMics; k++) {
+    ArduinoFFT<double> currentFFT = mics[k].FFT;
 
-    for(int j=0; j<NumberOfMics; j++) {
-      vReal = mics[j].vReal;
-      vImag = mics[j].vImag;
-    
-      FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward);	// a bunch of different modes, this seems to be standard
-      FFT.compute(FFTDirection::Forward); // compute the fourier transform
+    currentFFT.windowing(FFTWindow::Hamming, FFTDirection::Forward);	// a bunch of different modes, this seems to be standard
+    currentFFT.compute(FFTDirection::Forward); // compute the fourier transform
+    // at this point the frequencies we have complex fourier values for are arranged like:
+    // frequency = index*sampling_rate/n_samples
+    // index = (frequency*n_samples)/sampling_rate
+    // for example, if sr=2000 n=1000, then vReal[100] is real component of 100*2000/1000 = 200Hz
 
-      // at this point the frequencies we have complex fourier values for are arranged like:
-      // frequency = index*sampling_frequency/n_samples
-      // for example, if sf=2000 n=1000, then vReal[100] is real component of 100*2000/1000 = 200Hz
+    currentFFT.complexToMagnitude(); //computes up to the nyquist frequency (first half of vReal becomes magnitudes)
 
-      FFT.complexToMagnitude(); //computes up to the nyquist frequency (first half of vReal becomes magnitudes)
-
-
+    Serial.println(mics[k].vReal[153]);
+      
   }
 }
 
 //       /\_/\
 //      ( o.o )
 //       > ^ <
+
+//      (
+//     `-`-.
+//     '( @ >
+//      _) (
+//     /    )
+//    /_,'  / 
+//      \  / 
+//   ===m""m===
